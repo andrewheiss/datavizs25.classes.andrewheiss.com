5caf25a2f5005e1bf0410faa9432d95d98950fc9
Andrew Heiss <andrew@andrewheiss.com>
2025-06-04 14:38:15 -0400

    Nicer deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,20 @@
+#!/usr/bin/env sh
+
 REMOTE_HOST="ath-cloud"
 REMOTE_DIR="~/sites/classes/datavizs25.classes/public"
 REMOTE_DEST=$REMOTE_HOST:$REMOTE_DIR
 
 echo "Uploading new changes to remote server..."
 echo
-rsync -crvP --delete _site/ $REMOTE_DEST
+rsync -crvP --exclude '*_cache' --delete _book/ $REMOTE_DEST
+
+if [ $? -eq 0 ]; then
+  echo
+  echo "... done!"
+  echo
+  echo "The site is live at https://datavizs25.classes.andrewheiss.com/"
+else
+  echo
+  echo "Error!"
+  exit 1
+fi
